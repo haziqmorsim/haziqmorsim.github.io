1356fe9514e9839aaad3ca9228262fb3a2b558ed
Haziq Morsim <haziqhusni97@gmail.com>
2025-11-10 17:17:34 +0800

    Updated email handler

--- email_handler.php
+++ email_handler.php
@@ -1,14 +1,26 @@
 <?php
-if(isset($POST['submit'])) {
-    $to = "haziqmorsim@gmail.com";
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $message  =$_POST['message'];
+// if(isset($POST['submit'])) {
+//     $to = "haziqmorsim@gmail.com";
+//     $from = $_POST['email'];
+//     $name = $_POST['name'];
+//     $subject = $_POST['subject'];
+//     $message  =$_POST['message'];
 
-    $headers = "From:" . $from;
-    $headers = "To:"  . $to;
-    mail($to, $subject, $message, $headers);
-    echo "Message was sent successfully."
+//     $headers = "From:" . $from;
+//     $headers = "To:"  . $to;
+//     mail($to, $subject, $message, $headers);
+//     echo "Message was sent successfully."
+// }
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "haziqmorsim@gmail.com";
+    $subject = $_POST["subject"];
+    $message = "Name: " . $_POST["name"] . "\nE-mail: " . $_POST["email"] . "\n\nMessage: " . $POST["message"];
+    $headers = "From: " . $_POST["email"];
+
+    if(mail($to, $subject, $message, $headers)) {
+        echo "Message was sent successfully.";
+    } else {
+        echo "Failed to send message.";
+    }
 }
 ?>
